9e67e200a8239677a136112dbea4e5ed7e4ab8c5
mhogo <mhogomchungu@gmail.com>
2011-09-14 10:41:17 -0400

    zuluCrypt-cli main function was getting too big, split its components into  functions

--- zuluCrypt-cli/zuluCrypt.c
+++ zuluCrypt-cli/zuluCrypt.c
@@ -17,7 +17,7 @@
  *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-
+#include <sys/stat.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -65,22 +65,199 @@ void help( void )
 	printf("will write docs at some point\n");	
 }
 
+int volume_info( char * mapping_name )
+{
+	struct stat st ;
+	char x[512] ;
+	int Z ;
+	
+	StrHandle *s = StringCpy("/dev/mapper/zuluCrypt-") ;
+	StringCat( s, mapping_name ) ;
+	
+	if( stat( StringCont( s ),&st) == 0 ){			
+		status(mapping_name,x ,512 ) ;
+		printf("%s",x);
+		Z = 0 ;
+	}else{
+		printf("No opened volume associated with provided name\n") ;
+		Z = 1 ;
+	}
+	
+	StringDelete( s ) ;
+	
+	return Z ;	
+}
+
+int close_opened_volume( char * mapping_name )
+{	
+	int st = close_volume( mapping_name ) ;
+	switch( st ) {
+	case 0 : printf("SUCCESS: volume successfully closed\n");
+		break ;
+	case 1 : printf("ERROR: close failed, encrypted volume with that name does not exist\n");
+		break ;
+			
+	case 2 : printf("ERROR: close failed, the mount point and/or one or more files are in use\n");
+		break ;
+			
+	default :
+		; //shouldnt get here			
+	}	
+	return st ;	
+}
+
+int open_volumes(int argn, char * device, char * mapping_name,int id, char * mount_point, char * mode, char * pass)
+{
+	StrHandle * p ;
+	int st ;
+	if ( argn == 5 ){
+		printf( "Enter passphrase: " ) ;
+
+		p = get_passphrase();				
+		
+		printf("\n") ;			
+	}else if ( argn == 6 ){
+
+		p = StringCpy( pass ) ;
+			
+	}
+	else{
+		printf("ERROR: Wrong number of arguments, run zuluCrypt with \"-h\" for help\n");
+		return 5 ;			
+	}	
+		
+	if (strncmp(mount_point,",\0",2)==0){
+			
+		printf("ERROR, \",\"(comma) is not a valid mount point\n") ;
+		return 6 ;			
+	}		
+		
+	if (strncmp(mode,"ro",2) != 0){
+		if (strncmp(mode,"rw",2) != 0){
+			printf("ERROR: Wrong mode, run zuluCrypt with \"-h\" for help\n");;
+			return 1 ;	
+		}
+	}		
+	
+	if (strncmp(mount_point,"-\0",2)==0){
+							
+	st = open_volume(device, mapping_name,NULL,id,mode,StringCont( p )) ;
+		
+	}else {
+			
+		st = open_volume(device, mapping_name,mount_point,id,mode,StringCont( p )) ;									
+	}
+		
+	switch ( st ){
+			
+	case 0 : printf("SUCCESS: Volume opened successfully\n");
+		break ;
+			
+	case 1 : printf("ERROR: No free loop device to use\n") ; 
+		break ;			
+			
+	case 2 : printf("ERROR: There seem to be an open volume accociated with given address\n");
+		break ;			
+			
+	case 3 : printf("ERROR: No file exist on given path\n") ; 
+		break ;
+			
+	case 4 : printf("ERROR: Wrong passphrase\n");
+		break ;
+			
+	case 5 : printf("ERROR: a file or folder already exist at mount point\n") ;
+		break ;
+	default :
+		;			
+	}
+		
+	StringDelete( p ) ;
+	return st ;
+}
+
+
+int create_volumes(int argn ,char *device, char *fs, char * mode, char * pass )
+{
+	int flush_the_stdin_buffer ;
+	StrHandle * p ;
+	StrHandle * q ;
+	char x[512] ;
+	char Y ;
+	int st ;
+	
+	if( realpath(device, x ) == NULL )
+		return 4 ;		
+		
+	if(strncmp(x,"/dev/",5) == 0 ){
+		printf("currently not supporting creating volumes on hard drives.\n") ;
+		printf("This is an suid program and without any checks,this functionality\nwill enable ");
+		printf("normal users to effectively delete a partition like roots.\n");
+			
+		return 6 ;
+	}
+			 
+	if( argn == 5 ){
+		printf("ARE YOU SURE YOU WANT TO CREATE/OVERWRITE: \"%s\" ? Type \"Y\" if you are\n",device);
+		scanf("%c",&Y);
+		
+		if ( Y != 'Y')
+			return 3 ;
+			
+		//flush the buffer stdin buffer	
+		while ((flush_the_stdin_buffer = getchar()) != '\n' && flush_the_stdin_buffer != EOF);
+			
+		printf("Enter passphrase: ") ;
+
+		p = get_passphrase();
+			
+		printf("\nRe enter passphrase: ") ;
+
+		q = get_passphrase();	
+			
+		printf("\n") ;
+			
+		if(strcmp(StringCont( p ),StringCont( q )) != 0){
+			printf("\nERROR: passphrases do not match\n") ;
+			printf("%s %d\n",StringCont( p ),StringLength( p ));
+			printf("%s %d\n",StringCont( q ),StringLength( q ));
+				
+			StringDelete( q ) ;
+			StringDelete( p ) ;
+			return 3 ;
+		}		
+			
+	}else if ( argn == 6 ){
+		p = StringCpy( pass ) ;								
+	}else{
+		printf("ERROR: Wrong number of arguments, run zuluCrypt with \"-h\" for help\n");
+		return 4 ;
+	}		
+	st = create_volume(device,fs,mode,StringCont( p )) ;;
+		
+	if ( st == 1 ){
+		printf("ERROR: File path given does not point to a file or partition\n") ;
+	}else if ( st == 2 ){	
+		printf("ERROR: Unrecognized volume type.\n");
+	}	
+	
+	if ( argn == 5 ){
+		StringDelete( p ) ;	
+		StringDelete( q ) ;	
+	}else
+		StringDelete( p ) ;	
+	
+	return st ;
+}
+
 int main( int argc , char *argv[])
 {
 	char * action = argv[1] ;
 	char * device = argv[2] ;
-	char * mount_point ;
-	char * mode ;
-	char * c ;
-	char Y ;
+
 	uid_t id ;
-	int st = 0 ;
-	int flush_the_stdin_buffer ;
-	char x[256] ;
-	char *  mapping_name ;
 	
-	StrHandle * p ;
-	StrHandle * q ;
+	char *  mapping_name ;
+	char * c ;
 	
 	id = getuid();	
 	
@@ -105,153 +282,21 @@ int main( int argc , char *argv[])
 
 	if ( strcmp( action, "status" ) == 0 ){			
 
-		status(mapping_name,x ,256 ) ;
-		printf("%s",x);		
+		return volume_info( mapping_name ) ;
 		
 	}else if ( strcmp( action, "close" ) == 0 ){			
 
-		st = close_volume( mapping_name ) ;
-		switch( st ) {
-			case 0 : printf("SUCCESS: volume successfully closed\n");
-			break ;
-			case 1 : printf("ERROR: close failed, encrypted volume with that name does not exist\n");
-			break ;
-			
-			case 2 : printf("ERROR: close failed, the mount point and/or one or more files are in use\n");
-			break ;
-			
-			default :
-				; //shouldnt get here			
-		}
-		return st ;
+		return close_opened_volume( mapping_name ) ;
 		
 	}else if ( strcmp( action, "open" ) == 0 ){
 		
-		if ( argc == 5 ){
-			printf( "Enter passphrase: " ) ;
-
-			p = get_passphrase();				
-		
-			printf("\n") ;			
-		}else if ( argc == 6 ){
-
-			p = StringCpy( argv[5] ) ;
-			
-		}
-		else{
-			printf("ERROR: Wrong number of arguments, run zuluCrypt with \"-h\" for help\n");
-			return 5 ;			
-		}
-			
-		mount_point = argv[3] ;
-		
-		if (strncmp(mount_point,",\0",2)==0){
-			
-			printf("ERROR, \",\"(comma) is not a valid mount point\n") ;
-			return 6 ;			
-		}		
-		mode = argv[4];
-		
-		if (strncmp(mode,"ro",2) != 0){
-			if (strncmp(mode,"rw",2) != 0){
-				printf("ERROR: Wrong mode, run zuluCrypt with \"-h\" for help\n");;
-				return 1 ;	
-			}
-		}		
-		
-		if (strncmp(mount_point,"-\0",2)==0){
-							
-			st = open_volume(device, mapping_name,NULL,id,mode,StringCont( p )) ;
-		
-		}else {
-			
-			st = open_volume(device, mapping_name,mount_point,id,mode,StringCont( p )) ;									
-		}
-		
-		StringDelete( p ) ;
-		
-		switch ( st ){
-			
-			case 0 : printf("SUCCESS: Volume opened successfully\n");
-			break ;
-			
-			case 1 : printf("ERROR: No free loop device to use\n") ; 
-			break ;			
-			
-			case 2 : printf("ERROR: There seem to be an open volume accociated with given address\n");
-			break ;			
-			
-			case 3 : printf("ERROR: No file exist on given path\n") ; 
-			break ;
-			
-			case 4 : printf("ERROR: Wrong passphrase\n");
-			break ;
-			
-			case 5 : printf("ERROR: a file or folder already exist at mount point\n") ;
-			default :
-				;			
-		}
+		return open_volumes(argc,argv[2],mapping_name,id,argv[3],argv[4],argv[5] ) ;		
 		
 	}else if(strcmp(action,"create") == 0 ){
-		
-		if( realpath(device, x ) == NULL )
-			return 4 ;		
-		
-		if(strncmp(x,"/dev/",5) == 0 ){
-			printf("currently not supporting creating volumes of hard drives.\n") ;
-			printf("This is an suid program and without any checks,this functionality\nwill enable ");
-			printf("normal users to effectively delete a partition like roots.\n");
-			
-			return 6 ;
-		}
-			 
-		if( argc == 5 ){
-			printf("ARE YOU SURE YOU WANT TO CREATE/OVERWRITE: \"%s\" ? Type \"Y\" if you are\n",device);
-			scanf("%c",&Y);
-		
-			if ( Y != 'Y')
-				return 3 ;
-			
-			//flush the buffer stdin buffer	
-			while ((flush_the_stdin_buffer = getchar()) != '\n' && flush_the_stdin_buffer != EOF);
-			
-			printf("Enter passphrase: ") ;
 
-			p = get_passphrase();
-			
-			printf("\nRe enter passphrase: ") ;
-
-			q = get_passphrase();	
-			
-			printf("\n") ;
-			
-			if(strcmp(StringCont( p ),StringCont( q )) != 0){
-				printf("\nERROR: passphrases do not match\n") ;
-				printf("%s %d\n",StringCont( p ),StringLength( p ));
-				printf("%s %d\n",StringCont( q ),StringLength( q ));
-				
-				StringDelete( q ) ;
-				StringDelete( p ) ;
-				return 3 ;
-			}		
-			
-		}else if ( argc == 6 ){
-			p = StringCpy( argv[5] ) ;								
-		}else{
-			printf("ERROR: Wrong number of arguments, run zuluCrypt with \"-h\" for help\n");
-			return 4 ;
-		}		
-		st = create_volume(device,argv[3],argv[4],StringCont( p )) ;
-			
-		StringDelete( p ) ;
-		StringDelete( q ) ;
-		
-		if ( st == 1 ){
-			printf("ERROR: File path given does not point to a file or partition\n") ;
-		}else if ( st == 2 ){	
-			printf("ERROR: Unrecognized volume type.\n");
-		}	
+		return create_volumes(argc ,argv[2],argv[3],argv[4],argv[5] ) ;
 	}	
 	
-	return st ;
+	return 0 ; //shouldnt get here
+		
 }
