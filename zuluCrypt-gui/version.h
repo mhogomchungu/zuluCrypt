99cf5c028aad6ca10a805a9ce4d58ab2b8fbcf72
mhogo mchungu <mhogomchungu@gmail.com>
2011-10-18 12:45:35 -0400

    bump version number to 4.0.0 and update changelog

--- zuluCrypt-gui/version.h
+++ zuluCrypt-gui/version.h
@@ -21,6 +21,6 @@
 
 #define ZULUCRYPT_VERSION
 
-#define VERSION_STRING "version 4.0-git of zuluCrypt, a front end to cryptsetup.\n(c) 2011 mhogo mchungu\nmhogomchungu@gmail.com"
+#define VERSION_STRING "version 4.0.0 of zuluCrypt, a front end to cryptsetup.\n\n(c) 2011 mhogo mchungu\nmhogomchungu@gmail.com\n"
 
 #endif
