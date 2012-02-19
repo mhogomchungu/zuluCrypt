abdd82da49ef1b1834e809adc0b9409234bcd3aa
mhogo mchungu <mhogomchungu@gmail.com>
2012-02-19 11:13:32 -0500

    unreleted code changes to the project

--- zuluCrypt-cli/string/StringList.c
+++ zuluCrypt-cli/string/StringList.c
@@ -23,37 +23,83 @@
 stringList_t StringList( const char * cstring )
 {
 	stringList_t stl = ( stringList_t ) malloc( sizeof( struct StringListType ) ) ;	
+	if( stl == NULL )
+		return NULL ;
 	stl->stp = ( string_t * ) malloc ( sizeof ( struct StringType ) ) ;
-	stl->size = 1 ;
+	if( stl->stp == NULL )
+	{
+		free( stl ) ;
+		return NULL ;
+	}	
 	stl->stp[0] = String( cstring ) ;
+	if( stl->stp[0] == NULL )
+	{
+		free( stl->stp );
+		free( stl ) ;
+		return NULL ;
+	}
+	stl->size = 1 ;
 	return stl ;
 }
 
 stringList_t StringListWithSize( char ** c, size_t s )
 {
 	stringList_t stl = ( stringList_t ) malloc( sizeof( struct StringListType ) ) ;	
+	if( stl == NULL )
+		return NULL ;
 	stl->stp = ( string_t * ) malloc ( sizeof ( struct StringType ) ) ;
-	stl->size = 1 ;	
+	if( stl->stp == NULL )
+	{
+		free( stl ) ;
+		return NULL ;
+	}	
 	stl->stp[0] = StringInheritWithSize( c,s ) ;
+	if( stl->stp[0] == NULL )
+	{
+		free( stl->stp );
+		free( stl ) ;
+		return NULL ;
+	}
+	stl->size = 1 ;
 	return stl ;
 }
 
 stringList_t StringListAppendWithSize( stringList_t stl,char ** c, size_t s )
 {
+	string_t * p ;
+	string_t q ;
 	if( stl == NULL )
 		return StringListWithSize( c,s ) ;
-	stl->stp = realloc( stl->stp,sizeof( struct StringType ) * ( stl->size + 1 ) ) ;		
-	stl->stp[ stl->size ] = StringInheritWithSize( c,s ) ;	
+	q = StringInheritWithSize( c,s ) ;
+	if( q == NULL )
+		return NULL ;
+	p = realloc( stl->stp,sizeof( struct StringType ) * ( stl->size + 1 ) ) ;
+	if( p == NULL )
+	{
+		StringDelete( &q ) ;
+		return NULL ;
+	}
+	stl->stp = p ;
+	stl->stp[ stl->size ] = q ;
 	stl->size = stl->size + 1 ;	
 	return stl ;
 }
 
 stringList_t StringListAppendSize( stringList_t stl,const char * cstring,size_t len ) 
 {
+	stringList_t p ;
 	char * c = ( char * )malloc( sizeof( char ) * ( len + 1 ) ) ;
+	if( c == NULL )
+		return stl ;
 	*( c + len ) = '\0' ;
 	memcpy( c,cstring,len ) ;
-	return StringListAppendWithSize( stl,&c,len ) ;
+	p = StringListAppendWithSize( stl,&c,len ) ;
+	if( p == NULL )
+	{
+		free( c ) ;
+		return NULL ;
+	}else
+		return p ;
 }
 
 stringList_t StringListSplit( const char * cstring,const char * splitter ) 
@@ -73,13 +119,22 @@ stringList_t StringListSplit( const char * cstring,const char * splitter )
 		if( d == NULL )
 		{			
 			stl = StringListAppend( stl,b ) ;
+			if( stl == NULL )
+				return NULL ;
 			break ;
 		}else{
 			len = d - b ;
 			e = ( char * ) malloc( sizeof( char ) * ( len + 1 ) ) ;
+			if( e == NULL )
+				return NULL;
 			memcpy( e,b,len ) ;
 			*( e + len ) = '\0' ;
 			stl = StringListAppendWithSize( stl,&e,len );
+			if( stl == NULL )
+			{
+				free( e ) ;
+				return NULL ;
+			}
 			b = d + sp_len ;
 		}
 	}
@@ -98,23 +153,52 @@ const char * StringListContentAt( stringList_t stl,size_t index )
 
 stringList_t StringListInsertAt( stringList_t stl,const char * cstring,size_t index ) 
 {
+	string_t * p ;
+	string_t q ;
 	size_t size = sizeof( struct StringType ) ;	
-	stl->stp = realloc( stl->stp, size * ( stl->size + 1 ) ) ;
+	q = String( cstring ) ;
+	if( q == NULL )
+		return NULL ;
+	p = realloc( stl->stp, size * ( stl->size + 1 ) ) ;
+	if( p == NULL )
+	{
+		StringDelete( &q ) ;
+		return NULL ;
+	}
+	stl->stp = p ;
 	memmove( stl->stp + index + 1,stl->stp + index,size * ( stl->size - index ) ) ;	
-	stl->stp[index] = String( cstring ) ;	
+	stl->stp[index] = q ;	
 	stl->size = stl->size + 1 ;	
 	return stl ;	
 }
 
 stringList_t StringListInsertAtSize( stringList_t stl,const char * cstring,size_t len,size_t index ) 
 {
+	char * c ;
+	string_t * p ;
+	string_t q ;
 	size_t size = sizeof( struct StringType ) ;	
-	char * c = ( char * ) malloc( sizeof( char ) * ( len + 1 ) ) ;
+	
+	c = ( char * ) malloc( sizeof( char ) * ( len + 1 ) ) ;
+	if( c == NULL )
+		return NULL ;
+	q = StringInheritWithSize( &c,len ) ;
+	if( q == NULL )
+	{
+		free( c ) ;
+		return NULL ;
+	}
+	p = realloc( stl->stp, size * ( stl->size + 1 ) ) ;
+	if( p == NULL )
+	{
+		StringDelete( &q ) ;
+		return NULL ;
+	}
 	memcpy( c,cstring,len );
 	*( c + len ) = '\0' ;
-	stl->stp= realloc( stl->stp, size * ( stl->size + 1 ) ) ;
+	stl->stp = p ;
 	memmove( stl->stp + index + 1,stl->stp + index,size * ( stl->size - index ) ) ;	
-	stl->stp[index] = StringInheritWithSize( &c,len ) ;	
+	stl->stp[index] = q ; 	
 	stl->size = stl->size + 1 ;	
 	return stl ;	
 }
@@ -122,11 +206,20 @@ stringList_t StringListInsertAtSize( stringList_t stl,const char * cstring,size_
 stringList_t StringListPrependSize( stringList_t stl,const char * cstring,size_t len ) 
 {
 	char * c ;
+	stringList_t p ;
 	if( stl == NULL ){
 		c = ( char * ) malloc( sizeof( char ) * ( len + 1 ) ) ;
+		if( c == NULL )
+			return NULL ;
 		memcpy( c,cstring,len ) ;
 		*( c + len ) = '\0' ;
-		return StringListWithSize( &c,len ) ;
+		p = StringListWithSize( &c,len ) ;
+		if( p == NULL )
+		{
+			free( c ) ;
+			return NULL ;
+		}else
+			return p ;
 	}
 	else
 		return StringListInsertAtSize( stl,cstring,len,0 ) ;
@@ -142,10 +235,19 @@ stringList_t StringListPrepend( stringList_t stl,const char * cstring )
 
 stringList_t StringListAppend( stringList_t stl,const char * cstring ) 
 {
+	string_t q ;
 	if( stl == NULL )
 		return StringList( cstring ) ;
+	q = String( cstring ) ;
+	if( q == NULL )
+		return NULL ;
 	stl->stp = realloc( stl->stp,sizeof( struct StringType ) * ( stl->size + 1 ) ) ;
-	stl->stp[ stl->size ] = String( cstring ) ;	
+	if( stl->stp == NULL )
+	{
+		StringDelete( &q ) ;
+		return NULL ;
+	}
+	stl->stp[ stl->size ] = q ;	
 	stl->size = stl->size + 1 ;	
 	return stl ;	
 }
@@ -203,7 +305,14 @@ stringList_t StringListCopy( stringList_t stl )
 	size_t i ;
 	size_t j = stl->size ;
 	stringList_t stx = ( stringList_t ) malloc( sizeof( struct StringListType ) ) ;
-	stx->stp = ( string_t * ) malloc( sizeof( struct StringType ) * j ) ;	
+	if( stx == NULL )
+		return NULL ;
+	stx->stp = ( string_t * ) malloc( sizeof( struct StringType ) * j ) ;
+	if( stx->stp == NULL )
+	{
+		free( stx ) ;
+		return NULL ;
+	}
 	stx->size = stl->size ;	
 	for( i = 0 ; i < j ; i++)
 		stx->stp[i] = StringWithSize( stl->stp[i]->string,stl->stp[i]->size ) ;
