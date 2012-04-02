6e281816cef16a638669bf989b30dcad1fadb3ae
mhogo mchungu <mhogomchungu@gmail.com>
2012-04-02 03:48:27 -0400

    make man and help more user friendly

--- zuluCrypt-cli/bin/main.c
+++ zuluCrypt-cli/bin/main.c
@@ -84,7 +84,37 @@ void help( void )
 {
 	const char * help = "\
 usage: zuluCrypt-cli < operation > < options specific to the operation >\n\
-operation list\n\
+meaning of symbols:\n\
+[] = required option\n\
+() = optional argument,default exists\n\
+*  = default option\n\
+/  = alternatives for the same option\n\
+{}  = not allowed option\n\
+\n\
+zuluCrypt-cli -o [ -d ] [ -m ] ( -e ) [ -p/-f/-h ]\n\
+zuluCrypt-cli -O [ -d ] { -m } ( -e ) [ -p/-f/-h ]\n\
+zuluCrypt-cli -q [ -d ]\n\
+zuluCrypt-cli -i [ -d ]\n\
+zuluCrypt-cli -c [ -d ] [ -p/-f /-h ] ( -k ) ( -t ) ( -z  ) ( -g )\n\
+zuluCrypt-cli -r [ -d ] [ -p/-f/-h ] \n\
+zuluCrypt-cli -a [ -d ] [ [ -y/-u ] [ -l/-n ] ]/[ -h ]\n\
+zuluCrypt-cli -b [ -d ]\n\
+zuluCrypt-cli -w [ -d ]   d argument must be something like: UUID=\"2468d6a7-9a71-4312-8bd9-662f982fade5\" ( or without quotes )\n\
+zuluCrypt-cli -D [ -d ]   d device must be mapper path at /dev/mapper/\n\
+zuluCrypt-cli -A\n\
+zuluCrypt-cli -S\n\
+zuluCrypt-cli -N\n\
+zuluCrypt-cli -N\n\
+\n\
+examples:\n\
+create volume: zuluCrypt-cli -c -d /dev/sdc1 -z ext4 -t luks -p xxx\n\
+open volume  : zuluCrypt-cli -o -d /dev/sdc1 -m ~/sdc1 -e ro -p xxx\n\
+close volume ; zuluCrypt-cli -q -d /dev/sdc1\n\
+remove key   ; zuluCrypt-cli -r -d /dev/sdc1 -p xxx\n\
+add key      : zuluCrypt-cli -a -d /dev/sdc1 -y xxx -l yyy\n\
+get device path from mapper  : zuluCrypt-cli -D -d /dev/mapper/zuluCrypt-sdc1\n\
+check if partition with UUID is present : zuluCrypt-cli -w -d UUID=\"d2d210b8-0b1f-419f-9172-9d509ea9af0c\"\n\
+operation list\n\n\
 -c         create an encrypted volume\n\
 -o         open and encrypted volume\n\
 -O         open an encrypted volume but do not mount it( -m therefore not needed )\n\
@@ -99,31 +129,23 @@ operation list\n\
 -S         print a list of system partitions on the system( partitions with active entries in /etc/fstab and /etc/crypttab\n\
 -w         check if UUID matches UUID of any partition\n\
 -D         get device path from mapper( located at /dev/mapper )\n\
+-L         print a list of all opened volumes and their mount point.The list is not formatted\n\
 \n\
 options that goes with above operations:\n\
--k         dont ask for confirmation when doing dangerous operations(used by -c and -r)\n\
--d         path to a file or partition with encrypted volume( required by all except by -v )\n\
--m         a folder will be created at this path to be used as mount point when opening volume( required by -o )\n\
--z         file system type(ext2,ext3 etc) (required by -c)\n\
--t         type of volume(plain or luks)   (required by -c)\n\
--g         random number generator (/dev/random or /dev/urandom) ( used by -c,/dev/urandom is used if absent )\n\
--h         get passphrase interactively,if absent then -p or -f must be used ( used with -c,-r,-a)\n\
--p         passphrase (used with -c and -r)\n\
--f         passphrase is in a key file (used with -c and -r)\n\
--y         passphrase already in the volume (required by -a if -u is absent and -h is also absent)\n\
--u         passphrase in a key file to be checked against a key already in the volume (required by -a if -y is absent and -h is also absent)\n\
--l         passphrase to be added (required by -a if -n is absent and -h is also absent)\n\
--n         passphrase in a keyfile to be added (required by -a if -l is absent and -h is also absent)\n\
-\n\
-examples:\n\
-create volume: zuluCrypt-cli -c -d /dev/sdc1 -z ext4 -t luks -p xxx\n\
-open volume  : zuluCrypt-cli -o -d /dev/sdc1 -m ~/sdc1 -e ro -p xxx\n\
-close volume ; zuluCrypt-cli -q -d /dev/sdc1\n\
-remove key   ; zuluCrypt-cli -r -d /dev/sdc1 -f xxx\n\
-add key      : zuluCrypt-cli -a -d /dev/sdc1 -y xxx -l yyy\n\
-get device path from mapper  : zuluCrypt-cli -D -d /dev/mapper/zuluCrypt-sdc1\n\
-check if partition with UUID is present : zuluCrypt-cli -w -d UUID=\"d2d210b8-0b1f-419f-9172-9d509ea9af0c\"\n\
-";
+-e         mode for opening volumes(ro*/rw)\n\
+-k         do not ask for confirmation when doing dangerous operations\n\
+-d         path to a file or partition with encrypted volume\n\
+-m         path to where mount point folder will be created\n\
+-z         file system type installed(ext2,ext3,ext4* etc)\n\
+-t         type of volume (plain/luks*)\n\
+-g         random number generator (/dev/random or /dev/urandom*)\n\
+-h         get passphrase interactively\n\
+-p         passphrase \n\
+-f         path to keyfile\n\
+-y         passphrase already in the volume(required by -a if -u is absent and -h is also absent)\n\
+-u         path to keyfile with passphrase already in the volume(required by -a if -y is absent and -h is also absent)\n\
+-l         passphrase to be added(required by -a if -n is absent and -h is also absent)\n\
+-n         path to keyfile with a passphrase to be added (required by -a if -l is absent and -h is also absent)\n";
 	printf( "%s\n",help ) ;
 }
 
@@ -136,8 +158,8 @@ void get_opts( int argc , char *argv[],struct_opts * stopts )
 	stopts->mode = "ro" ;
 	stopts->key_source = NULL ;
 	stopts->key = NULL ;
-	stopts->fs = NULL ;
-	stopts->type = NULL ;
+	stopts->fs = "ext4" ;
+	stopts->type = "luks" ;
 	stopts->rng = "/dev/urandom" ;
 	stopts->existing_key_source = NULL ;
 	stopts->existing_key = NULL ;
