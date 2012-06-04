b6ac5280d69b380e863262e435d9c6ddeedd7ccd
mhogo mchungu <mhogomchungu@gmail.com>
2012-06-04 19:04:59 -0400

    ui tweaks

--- zuluCrypt-gui/zulucrypt.cpp
+++ zuluCrypt-gui/zulucrypt.cpp
@@ -385,14 +385,14 @@ void zuluCrypt::aboutMenuOption(void)
 name : mhogo mchungu\n\
 Copyright 2011,2012\n\
 email: mhogomchungu@gmail.com\n\n\
-This program is free software: you can redistribute it and/or modify\
-it under the terms of the GNU General Public License as published by\
-the Free Software Foundation, either version 2 of the License, or\
+This program is free software: you can redistribute it and/or modify \
+it under the terms of the GNU General Public License as published by \
+the Free Software Foundation, either version 2 of the License, or \
 ( at your option ) any later version.\n\
 \n\
 This program is distributed in the hope that it will be useful,\
-but WITHOUT ANY WARRANTY; without even the implied warranty of\
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the\
+but WITHOUT ANY WARRANTY; without even the implied warranty of \
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the \
 GNU General Public License for more details.\n\
 \n\
 You should have received a copy of the GNU General Public License\n\
