8b639cf39f5902305bebb05450d952b89e448abe
mhogo mchungu <mhogomchungu@gmail.com>
2013-06-22 18:42:41 -0400

    remove an unneeded step in the build system

--- PostInstall.cmake
+++
@@ -1,8 +0,0 @@
-
-#
-# at install time create "zulucrypt-write" and "zulucrypt-read" groups
-#
-
-EXEC_PROGRAM( /usr/sbin/groupadd  ARGS -f zulucrypt OUTPUT_VARIABLE foo RETURN_VALUE bar )
-EXEC_PROGRAM( /usr/sbin/groupadd  ARGS -f zulumount OUTPUT_VARIABLE foo RETURN_VALUE bar )
-EXEC_PROGRAM( /usr/sbin/groupadd  ARGS -f zulucrypt-write OUTPUT_VARIABLE foo RETURN_VALUE bar )
