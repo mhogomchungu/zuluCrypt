9a8c4541f207cb66e11a10d9a1cc3b95ea13d46b
mhogo mchungu <mhogomchungu@gmail.com>
2013-09-23 01:45:51 -0400

    fix a bug

--- zuluMount-gui/mainwindow.cpp
+++ zuluMount-gui/mainwindow.cpp
@@ -417,12 +417,7 @@ void MainWindow::showContextMenu( QTableWidgetItem * item,bool itemClicked )
 		}else{
 			m_sharedFolderPath = utility::sharedMountPointPath( mt ) ;
 			if( m_sharedFolderPath.isEmpty() ){
-				if( mt.startsWith( QString( "/run/media/private/" ) ) ){
-					m.addAction( tr( "no available options for this volume" ) ) ;
-				}else{
-					connect( m.addAction( tr( "open folder" ) ),SIGNAL( triggered() ),
-						 this,SLOT( slotOpenFolder() ) ) ;
-				}
+				m.addAction( tr( "no available options for this volume" ) ) ;
 			}else{
 				connect( m.addAction( tr( "open shared folder" ) ),SIGNAL( triggered() ),
 					 this,SLOT( slotOpenSharedFolder() ) ) ;
