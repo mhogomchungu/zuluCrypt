77bc5b51b58eb52b650a55595382781103e1d5a7
Francis Banyikwa <mhogomchungu@gmail.com>
2015-05-13 19:20:05 +0300

    fix a build issue on old versions of cryptsetup

--- zuluCrypt-cli/lib/close_mapper.c
+++ zuluCrypt-cli/lib/close_mapper.c
@@ -17,8 +17,9 @@
  *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-#include <libcryptsetup.h>
 #include <unistd.h>
+#include <stddef.h>
+#include <libcryptsetup.h>
 
 int zuluCryptCloseMapper( const char * mapper )
 {
